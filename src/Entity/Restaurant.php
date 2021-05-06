6fa491d4846b1ab8dddd3a025e021cb28a6e8342
FGiordanengo <florent.giordanengo@gmail.com>
2021-05-06 22:58:09 +0200

    first try on users architecture

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RestaurantRepository::class)
+ */
+class Restaurant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=User::class, inversedBy="restaurants")
+     */
+    private $user;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Product::class, mappedBy="restaurant")
+     */
+    private $products;
+
+    /**
+     * @ORM\OneToMany(targetEntity=ExtraProduct::class, mappedBy="restaurant")
+     */
+    private $extraProducts;
+
+    public function __construct()
+    {
+        $this->user = new ArrayCollection();
+        $this->products = new ArrayCollection();
+        $this->extraProducts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUser(): Collection
+    {
+        return $this->user;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->user->contains($user)) {
+            $this->user[] = $user;
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        $this->user->removeElement($user);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setRestaurant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getRestaurant() === $this) {
+                $product->setRestaurant(null);
+            }
+        }
+
+        return $this;
+    }
+    /**
+     * @return string|null
+     */
+    public function __toString() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Collection|ExtraProduct[]
+     */
+    public function getExtraProducts(): Collection
+    {
+        return $this->extraProducts;
+    }
+
+    public function addExtraProduct(ExtraProduct $extraProduct): self
+    {
+        if (!$this->extraProducts->contains($extraProduct)) {
+            $this->extraProducts[] = $extraProduct;
+            $extraProduct->setRestaurant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExtraProduct(ExtraProduct $extraProduct): self
+    {
+        if ($this->extraProducts->removeElement($extraProduct)) {
+            // set the owning side to null (unless already changed)
+            if ($extraProduct->getRestaurant() === $this) {
+                $extraProduct->setRestaurant(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
